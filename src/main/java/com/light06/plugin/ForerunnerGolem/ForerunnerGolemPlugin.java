b3769fc9e69c0e9a94c1dacc1f43c33047bb34d3
Alexis Migniau <migniau.alexis@gmail.com>
2026-01-30 23:53:39 +0100

    fix(hud): Supprime l'HUD au changement de monde

--- src/main/java/com/light06/plugin/ForerunnerGolem/ForerunnerGolemPlugin.java
+++ src/main/java/com/light06/plugin/ForerunnerGolem/ForerunnerGolemPlugin.java
@@ -1,8 +1,11 @@
 package com.light06.plugin.ForerunnerGolem;
 
+import com.hypixel.hytale.server.core.event.events.player.AddPlayerToWorldEvent;
+import com.hypixel.hytale.server.core.event.events.player.DrainPlayerFromWorldEvent;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.light06.plugin.ForerunnerGolem.Commands.BossUiTestCommand;
+import com.light06.plugin.ForerunnerGolem.Listeners.AddPlayerFromWorldEventListener;
 import com.light06.plugin.ForerunnerGolem.Systems.BossTickingSystem;
 import javax.annotation.Nonnull;
 
@@ -15,5 +18,7 @@ public class ForerunnerGolemPlugin extends JavaPlugin {
         super.setup();
         this.getCommandRegistry().registerCommand(new BossUiTestCommand("bosstest", "Test Boss UI", false));
         this.getEntityStoreRegistry().registerSystem(new BossTickingSystem());
+
+        this.getEventRegistry().registerGlobal(AddPlayerToWorldEvent.class, AddPlayerFromWorldEventListener::on);
     }
 }
---
+++ src/main/java/com/light06/plugin/ForerunnerGolem/Listeners/AddPlayerFromWorldEventListener.java
@@ -0,0 +1,24 @@
+package com.light06.plugin.ForerunnerGolem.Listeners;
+
+import com.hypixel.hytale.component.Holder;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.entity.entities.player.hud.HudManager;
+import com.hypixel.hytale.server.core.event.events.player.AddPlayerToWorldEvent;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import com.light06.plugin.ForerunnerGolem.UI.EmptyHudUI;
+
+public class AddPlayerFromWorldEventListener {
+    public static void on(AddPlayerToWorldEvent event) {
+        if (event.getWorld().getName().contains("forerunners")) {
+            return;
+        }
+
+        Holder<EntityStore> holder = event.getHolder();
+
+        Player player = holder.getComponent(Player.getComponentType());
+
+        HudManager hudManager = player.getHudManager();
+
+        hudManager.setCustomHud(player.getPlayerRef(), new EmptyHudUI(player.getPlayerRef()));
+    }
+}
